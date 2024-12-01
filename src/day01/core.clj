adc00611437b08b597bbeb8cdfe311ec3e67508e
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2024-12-01 14:57:10 +0100

    Day 1

--- src/day01/core.clj
+++ src/day01/core.clj
@@ -1,5 +1,27 @@
-(ns day01.core)
+(ns day01.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
 
-(defn -main[]
-  (println "Hello, World!"))
+(defn parse-line [line]
+  (map #(Integer/parseInt %) (string/split line #"\s+")))
 
+(defn parse-input []
+  (->> (io/resource "day01.txt")
+       slurp
+       string/split-lines
+       (map parse-line)
+       (apply map vector)))
+
+(defn part1 [left right]
+  (let [differences (map #(abs (- %1 %2)) (sort left) (sort right))]
+    (apply + differences)))
+
+(defn part2 [left right]
+  (let [counts (frequencies right)
+        similarities (map #(* % (counts % 0)) left)]
+    (apply + similarities)))
+
+(defn -main []
+  (let [[left right] (parse-input)]
+    (println "Part 1:" (part1 left right))
+    (println "Part 2:" (part2 left right))))
