76d9d8983d80be4b25f6fffcda90489aada1252a
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2024-12-14 14:55:30 +0100

    Day 7 refactoring.

--- src/day07/core.clj
+++ src/day07/core.clj
@@ -5,7 +5,7 @@
 
 (defn parse-line [line]
   (let [parts (string/split line #":? ")
-        numbers (map #(parse-long %) parts)]
+        numbers (map parse-long parts)]
     {:test-value (first numbers) :numbers (vec (rest numbers))}))
 
 (defn parse-input []
@@ -17,17 +17,18 @@
   (let [num-digits (int (inc (math/log10 b)))]
     (+ (* a (int (math/pow 10 num-digits))) b)))
 
-(defn can-be-true? [acc numbers test-value ops]
+(defn can-be-true? [test-value ops acc numbers]
   (cond
     (> acc test-value) false
     (and (= acc test-value) (empty? numbers)) true
     (empty? numbers) false
-    :else (let [[number & remaining] numbers]
+    :else (let [[number & remaining] numbers
+                can-be-true-partial? (partial can-be-true? test-value ops)]
             (not-empty
-              (filter identity (map (fn [op] (can-be-true? (op acc number) remaining test-value ops)) ops))))))
+              (filter identity (map #(can-be-true-partial? (apply % [acc number]) remaining) ops))))))
 
 (defn sum-of-valid-equations [equations ops]
-  (->> (filter (fn [{:keys [test-value numbers]}] (can-be-true? (first numbers) (rest numbers) test-value ops)) equations)
+  (->> (filter (fn [{:keys [test-value numbers]}] (can-be-true? test-value ops (first numbers) (rest numbers))) equations)
        (map :test-value)
        (apply +)))
 
