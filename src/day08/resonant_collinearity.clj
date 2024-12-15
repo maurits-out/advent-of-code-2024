577bcd2aac56fcbfd9f1a81cf6834facd1da166b
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2024-12-15 16:51:58 +0100

    Day 8 part 2.

--- src/day08/resonant_collinearity.clj
+++ src/day08/resonant_collinearity.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as string]))
 
-(defn extract-antennas [lines]
+(defn extract-antenna-locations [lines]
   (->> (mapcat
          (fn [[row-idx row]]
            (keep-indexed
@@ -18,30 +18,47 @@
 (defn parse-input []
   (let [input (slurp (io/resource "day08.txt"))
         lines (string/split-lines input)]
-    {:height (count lines) :width (count (first lines)) :antennas (extract-antennas lines)}))
+    {:height (count lines) :width (count (first lines)) :antennas (extract-antenna-locations lines)}))
 
 (defn is-within-bounds? [[row column] height width]
   (and (<= 0 row (dec height)) (<= 0 column (dec width))))
 
-(defn anti-node-from-antennas [[row1 col1] [row2 col2] height width]
-  (let [anti-node [(- (* 2 row2) row1) (- (* 2 col2) col1)]]
-    (when (is-within-bounds? anti-node height width)
-      anti-node)))
+(defn anti-nodes-for-antennas [from-antenna to-antenna height width]
+  (let [row-step (- (first to-antenna) (first from-antenna))
+        column-step (- (second to-antenna) (second from-antenna))]
+    (take-while #(is-within-bounds? % height width)
+                (iterate (fn [[row-idx col-idx]] [(+ row-idx row-step) (+ col-idx column-step)]) to-antenna))))
 
-(defn anti-nodes [coordinates height width]
-  (->> (for [from coordinates
-             to coordinates
+(defn two-anti-nodes [antenna-locations height width]
+  (->> (for [from antenna-locations
+             to antenna-locations
              :when (not= from to)]
-         (anti-node-from-antennas from to height width))
+         (second (anti-nodes-for-antennas from to height width)))
+       (filter identity)))
+
+(defn all-anti-nodes [antenna-locations height width]
+  (->> (for [from antenna-locations
+             to antenna-locations
+             :when (not= from to)]
+         (anti-nodes-for-antennas from to height width))
+       (mapcat identity)
        (filter identity)))
 
 (defn part1 [{:keys [height width antennas]}]
   (->> (mapcat
-         #(anti-nodes % height width)
+         #(two-anti-nodes % height width)
+         (map #(second %) antennas))
+       distinct
+       count))
+
+(defn part2 [{:keys [height width antennas]}]
+  (->> (mapcat
+         #(all-anti-nodes % height width)
          (map #(second %) antennas))
        distinct
        count))
 
 (defn -main []
   (let [input (parse-input)]
-    (println "Part 1:" (part1 input))))
+    (println "Part 1:" (part1 input))
+    (println "Part 2:" (part2 input))))
