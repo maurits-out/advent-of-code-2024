de5f36142714733f80848cefddff5788fd8c2275
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2024-12-24 17:11:48 +0100

    Day 11.

---
+++ src/day11/plutonian_pebbles.clj
@@ -0,0 +1,31 @@
+(ns day11.plutonian_pebbles
+  (:require [clojure.math :as math]))
+
+(def input [5178527 8525 22 376299 3 69312 0 275])
+
+(defn split [num num-digits]
+  (let [div (int (math/pow 10 (quot num-digits 2)))]
+    [(quot num div) (rem num div)]))
+
+(defn digit-count [num]
+  (int (inc (math/floor (math/log10 num)))))
+
+(def stone-count
+  (memoize (fn [stone times]
+             (cond
+               (zero? times) 1
+               (zero? stone) (stone-count 1 (dec times))
+               :else (let [num-digits (digit-count stone)]
+                       (if (even? num-digits)
+                         (stone-count (* stone 2024) (dec times))
+                         (->> (split stone num-digits)
+                              (map #(stone-count % (dec times)))
+                              (apply +))))))))
+
+(defn blink [times]
+  (->> (map #(stone-count % times) input)
+       (apply +)))
+
+(defn -main []
+  (println "Part 1:" (blink 25))
+  (println "Part 2:" (blink 75)))
