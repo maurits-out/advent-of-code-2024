701a31f409296fb4f2cdc136103b9a8a2af943c6
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2025-01-01 12:44:59 +0100

    Day 16 part 2.

--- src/day16/reindeer_maze.clj
+++ src/day16/reindeer_maze.clj
@@ -3,7 +3,9 @@
             [clojure.string :as string]
             [clojure.data.priority-map :refer [priority-map]]))
 
-(defn find-start [maze]
+(def directions [[0 1] [1 0] [0 -1] [-1 0]])
+
+(defn find-start-tile [maze]
   (some (fn [[row-idx row]]
           (some (fn [[col-idx char]]
                   (when (= char \S)
@@ -15,60 +17,88 @@
   (let [maze (->> (slurp (io/resource "day16.txt"))
                   (string/split-lines)
                   (mapv vec))
-        start (find-start maze)]
-    {:maze maze :start start}))
+        start (find-start-tile maze)]
+    {:maze maze :start-tile start}))
 
 (defn is-end-tile? [location maze]
   (= (get-in maze location) \E))
 
-(defn rotate-clockwise [[location direction] score]
+(defn rotate-clockwise [[location direction]]
   (let [updated-direction (case direction
                             :east :south
                             :south :west
                             :west :north
                             :north :east)]
-    [[location updated-direction] (+ score 1000)]))
+    [location updated-direction]))
 
-(defn rotate-counterclockwise [[location direction] score]
+(defn rotate-counterclockwise [[location direction]]
   (let [updated-direction (case direction
                             :east :north
                             :north :west
                             :west :south
                             :south :east)]
-    [[location updated-direction] (+ score 1000)]))
+    [location updated-direction]))
 
-(defn move-forward [[[row column] direction] score maze]
+(defn move-forward [[[row column] direction]]
   (let [updated-location (case direction
                            :east [row (inc column)]
                            :north [(dec row) column]
                            :west [row (dec column)]
                            :south [(inc row) column])]
-    (when (not= (get-in maze updated-location) \#)
-      [[updated-location direction] (inc score)])))
+    [updated-location direction]))
 
-(defn get-neighbors [node score maze]
-  (let [clockwise (rotate-clockwise node score)
-        counterclockwise (rotate-counterclockwise node score)
-        forward (move-forward node score maze)]
-    (if forward
+(defn move-backward [[[row column] direction]]
+  (let [updated-location (case direction
+                           :east [row (dec column)]
+                           :north [(inc row) column]
+                           :west [row (inc column)]
+                           :south [(dec row) column])]
+    [updated-location direction]))
+
+(defn get-outgoing-neighbors [node score maze]
+  (let [clockwise [(rotate-clockwise node) (+ score 1000)]
+        counterclockwise [(rotate-counterclockwise node) (+ score 1000)]
+        forward [(move-forward node) (inc score)]]
+    (if (not= (get-in maze (ffirst forward)) \#)
       [clockwise counterclockwise forward]
       [clockwise counterclockwise])))
 
-(defn filter-neighbors-with-lower-score [dist neighbors]
+(defn get-incoming-neighbors [node score maze]
+  (let [clockwise [(rotate-clockwise node) (- score 1000)]
+        counterclockwise [(rotate-counterclockwise node) (- score 1000)]
+        backward [(move-backward node) (dec score)]]
+    (if (not= (get-in maze (ffirst backward)) \#)
+      [clockwise counterclockwise backward]
+      [clockwise counterclockwise])))
+
+(defn filter-neighbors-with-better-score [dist neighbors]
   (filter (fn [[node score]] (let [current-score (dist node)]
                                (or (not current-score) (< score current-score)))) neighbors))
 
-(defn part1 [{:keys [maze start]}]
-  (let [start-node [start :east]]
+(defn dijkstra [{:keys [maze start-tile]}]
+  (let [start-node [start-tile :east]]
     (loop [dist {start-node 0}
            queue (priority-map start-node 0)]
       (let [[location :as node] (first (peek queue))]
         (if (is-end-tile? location maze)
-          (dist node)
-          (let [neighbors (->> (get-neighbors node (dist node) maze)
-                               (filter-neighbors-with-lower-score dist))]
+          {:dist dist :end-node node}
+          (let [neighbors (->> (get-outgoing-neighbors node (dist node) maze)
+                               (filter-neighbors-with-better-score dist))]
             (recur (into dist neighbors) (into (pop queue) neighbors))))))))
 
+(defn count-tiles [end-node dist maze]
+  (loop [queue [end-node]
+         tiles #{(first end-node)}]
+    (if-let [node (first queue)]
+      (let [neighbors (->> (get-incoming-neighbors node (dist node) maze)
+                           (filter (fn [n] (= (dist (first n)) (second n)))))
+            new-nodes (map (fn [n] (first n)) neighbors)
+            locations (map (fn [n] (first n)) new-nodes)]
+        (recur (into (rest queue) new-nodes) (into tiles locations)))
+      (count tiles))))
+
 (defn -main []
-  (let [input (parse-input)]
-    (println "Part 1:" (part1 input))))
+  (let [{:keys [maze] :as input} (parse-input)
+        {:keys [dist end-node]} (dijkstra input)]
+    (println "Part 1:" (dist end-node))
+    (println "Part 2:" (count-tiles end-node dist maze))))
