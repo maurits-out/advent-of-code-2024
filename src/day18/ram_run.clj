b7cdcf9075ff90359ebb592bda45d1c9db4ee579
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2025-01-11 14:43:55 +0100

    Day 18 part 2.

--- src/day18/ram_run.clj
+++ src/day18/ram_run.clj
@@ -3,6 +3,7 @@
             [clojure.string :as string]))
 
 (def size 70)
+(def size-first-batch 1024)
 (def start [0 0])
 (def target [size size])
 
@@ -15,8 +16,7 @@
        (slurp)
        (string/split-lines)
        (map parse-line)
-       (take 1024)
-       (set)))
+       (split-at size-first-batch)))
 
 (defn get-neighbors [[current-x current-y] corrupted-byte-locations]
   (->> [[current-x (dec current-y)]
@@ -27,17 +27,29 @@
                  (and (<= 0 x size) (<= 0 y size)
                       (not (contains? corrupted-byte-locations location)))))))
 
-(defn part1 [corrupted-byte-locations]
-  (loop [queue [[start 0]]
+(defn find-shortest-path-locations [corrupted-byte-locations]
+  (loop [queue [(list start)]
          visited #{start}]
-    (let [[location step-count] (first queue)]
+    (if-let [[location :as path] (first queue)]
       (if (= location target)
-        step-count
+        (set path)
         (let [neighbors (->> (get-neighbors location corrupted-byte-locations)
                              (filter #(not (contains? visited %))))
-              new-nodes (map (fn [n] [n (inc step-count)]) neighbors)]
-          (recur (into (subvec queue 1) new-nodes) (into visited neighbors)))))))
+              updated-paths (map #(conj path %) neighbors)]
+          (recur (into (subvec queue 1) updated-paths)
+                 (into visited neighbors)))))))
+
+(defn first-byte-to-block-exit [shortest-path-locations corrupted-byte-locations remaining]
+  (let [next-corrupted-location (first remaining)
+        new-corrupted-byte-locations (conj corrupted-byte-locations next-corrupted-location)]
+    (if (contains? shortest-path-locations next-corrupted-location)
+      (if-let [new-shortest-path-locations (find-shortest-path-locations new-corrupted-byte-locations)]
+        (recur new-shortest-path-locations new-corrupted-byte-locations (rest remaining))
+        (string/join "," next-corrupted-location))
+      (recur shortest-path-locations new-corrupted-byte-locations (rest remaining)))))
 
 (defn -main []
-  (let [corrupted-byte-locations (parse-input)]
-    (println "Part 1:" (part1 corrupted-byte-locations))))
+  (let [[first-batch second-batch] (parse-input)
+        shortest-path (find-shortest-path-locations (set first-batch))]
+    (println "Part 1:" (dec (count shortest-path)))
+    (println "Part 2:" (first-byte-to-block-exit shortest-path (set first-batch) second-batch))))
